2624f52033d37e45c4d9e466d8a80db24c16425b
VKTRN <vktr.ndns@gmail.com>
2022-06-19 23:04:52 +0200

    _

---
+++ src/components/Grid.js
@@ -0,0 +1,20 @@
+export const Grid = () => {
+  return (
+    <div className = 'grid'>
+      <div className = 'cell'>
+        <div className='cross'>
+          <div className='line1'></div>
+          <div className='line2'></div>
+        </div>
+      </div>
+      <div className = 'cell'></div>
+      <div className = 'cell'></div>
+      <div className = 'cell'></div>
+      <div className = 'cell'></div>
+      <div className = 'cell'></div>
+      <div className = 'cell'></div>
+      <div className = 'cell'></div>
+      <div className = 'cell'></div>
+    </div>
+  )
+}
