4f544713986e46294639e8d6756cb0a9de8e022c
VKTRN <vktr.ndns@gmail.com>
2022-06-28 22:33:21 +0200

    _

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
 import './App.css';
 import {Grid} from './components/Grid'
+import {useState} from 'react'
+import {useEffect} from 'react'
 
 function App() {
+
+  const nextTurn = () => {
+    return turn === "x" ? "o" : "x"
+  }
+
+  const checkWinner = () => {
+    const winningCombinations = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+
+    for(let i = 0; i < winningCombinations.length; i++){
+      const [a,b,c] = winningCombinations[i]
+      if(cells[a] && cells[a] === cells[b] && cells[a] === cells[c]){
+        return cells[a]
+      }
+    }
+
+    return null
+  }
+
+  const checkTie = () => {
+    const emptyCells = cells.filter(cell => !cell).length
+    return !emptyCells
+  }
+
+  const resetGame = () => {
+    setCells(Array(9).fill(""))
+    setScore({'x':0,'o':0})
+    setTurn('x')
+  }
+
+  const [score, setScore] = useState({'x':0,'o':0})
+  const [cells, setCells] = useState(Array(9).fill(""))
+  const [turn,  setTurn]  = useState('x')
+
+  useEffect(() => {
+    const winner      = checkWinner()
+    const tie         = checkTie()
+    
+    winner && setScore({...score, [winner]: score[winner] + 1});
+    (winner || tie) && setCells(Array(9).fill(""));
+    setTurn(nextTurn())
+
+  }, [cells])
+
+
   return (
     <div className="App">
-      <Grid/>
+      <div className = 'count'>
+        <span className = 'blue'>{score.x}</span> : <span className = 'red'>{score.o}</span>
+      </div>
+      <Grid cells = {cells} setCells = {setCells} turn = {turn}/>
+      <button onClick = {resetGame}>Reset</button>
     </div>
   );
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,82 +1,18 @@
 import {Cell} from './Cell'
-import {useState} from 'react'
-import {useEffect} from 'react'
 
-export const Grid = () => {
-
-  const [cells, setCells] = useState(["","","","","","","","",""])
-  const [gameCount, setGameCount] = useState({'x':0,'o':0})
-  const [turn, setTurn] = useState('x')
-
-  const getCurrentTurn = () => {
-    return turn === "x" ? "o" : "x"
-  }
-
-  // this function checks, if there is a winner
-  const checkWinner = () => {
-    const winningCombinations = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ]
-    for(let i = 0; i < winningCombinations.length; i++){
-      const [a,b,c] = winningCombinations[i]
-      if(cells[a] && cells[a] === cells[b] && cells[a] === cells[c]){
-        return cells[a]
-      }
-    }
-    return null
-  }
-
-  // this function checks, if there is a tie
-  const checkTie = () => {
-    if(cells.filter(cell => cell === "").length === 0){
-      return true
-    }
-    return false
-  }
-
-  // this function resets the game
-  const resetGame = () => {
-    setCells(["","","","","","","","",""])
-    setGameCount({'x':0,'o':0})
-    setTurn('x')
-  }
-
-  useEffect(() => {
-    const winner      = checkWinner()
-    const tie         = checkTie()
-    
-    winner && setGameCount({...gameCount, [winner]: gameCount[winner] + 1});
-    (winner || tie) && setCells(["","","","","","","","",""]);
-    const currentTurn = getCurrentTurn()
-    setTurn(currentTurn)
-
-  }, [cells])
-
+export const Grid = ({cells, setCells, turn}) => {
 
   return (
-    <div className = 'container'>
-      <div>
-        <span className = 'blue'>{gameCount.x}</span>:<span className = 'red'>{gameCount.o}</span>
-      </div>
-      <div className = 'grid'>
-        <Cell index={0} cells={cells} setCells={setCells} turn={turn}/>
-        <Cell index={1} cells={cells} setCells={setCells} turn={turn}/>
-        <Cell index={2} cells={cells} setCells={setCells} turn={turn}/>
-        <Cell index={3} cells={cells} setCells={setCells} turn={turn}/>
-        <Cell index={4} cells={cells} setCells={setCells} turn={turn}/>
-        <Cell index={5} cells={cells} setCells={setCells} turn={turn}/>
-        <Cell index={6} cells={cells} setCells={setCells} turn={turn}/>
-        <Cell index={7} cells={cells} setCells={setCells} turn={turn}/>
-        <Cell index={8} cells={cells} setCells={setCells} turn={turn}/>
-      </div>
-      <button onClick = {resetGame}>Reset</button>
+    <div className = 'grid'>
+      <Cell index={0} cells={cells} setCells={setCells} turn={turn}/>
+      <Cell index={1} cells={cells} setCells={setCells} turn={turn}/>
+      <Cell index={2} cells={cells} setCells={setCells} turn={turn}/>
+      <Cell index={3} cells={cells} setCells={setCells} turn={turn}/>
+      <Cell index={4} cells={cells} setCells={setCells} turn={turn}/>
+      <Cell index={5} cells={cells} setCells={setCells} turn={turn}/>
+      <Cell index={6} cells={cells} setCells={setCells} turn={turn}/>
+      <Cell index={7} cells={cells} setCells={setCells} turn={turn}/>
+      <Cell index={8} cells={cells} setCells={setCells} turn={turn}/>
     </div>
   )
 }
